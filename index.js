765146fb86f76929fc6b56ab6df7db97e385f9c6
lordeber15 <eberpalomino@hotmail.com>
2023-05-29 10:32:28 -0500

    deplo back listo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const allDataFunctions = require("./src/data/allDataFunctions.js");
 // Syncing all the models at once.
 const port = process.env.PORT || 3001;
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
   server.listen(port, async () => {
     console.log("%s listening at ", port); // eslint-disable-line no-console
     allDataFunctions();
