708e454765576fe523bdcb69fb28a34be23461e6
martin marchessi <niitram.mm@gmail.com>
2023-06-01 00:13:12 -0300

    modificado para deploy

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const allDataFunctions = require("./src/data/allDataFunctions.js");
+const port = process.env.PORT || 3001;
 // Syncing all the models at once.
 
-conn.sync({ alter: true }).then(() => {
-  server.listen(3001, async () => {
+conn.sync({ force: true }).then(() => {
+  server.listen(port, async () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
     allDataFunctions();
   });
